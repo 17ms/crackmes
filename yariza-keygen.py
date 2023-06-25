fba57e50f4456286b2b6abd9894576ac0fdff43e
17ms <79069176+17ms@users.noreply.github.com>
2023-06-25 21:25:51 +0300

    60c24a7133c5d410b8842d3c

---
+++ yariza-keygen.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Link: https://crackmes.one/crackme/60c24a7133c5d410b8842d3c
+# The algorithm simply checks pw[4]=='@' && pw[6]=='-' && pw[9]=='l' && pw[15]=='?'.
+
+import random
+import string
+
+for i in range(1, 51):
+    pw = random.choices(string.ascii_letters + string.digits, k=16)
+    pw[4] = "@"
+    pw[6] = "-"
+    pw[9] = "l"
+    pw[15] = "?"
+
+    print(f"{i}: {''.join(pw)}")
